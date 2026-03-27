61f6d200de78864812cfa44f07d0b4f2f56b7b33
TuanLee <23521711@gm.uit.edu.vn>
2026-03-27 23:25:41 +0700

    feat(attendance) : add AttendanceCheck action method

---
+++ code/be/School_Management.API/School_Management.API/Services/AttendanceService.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+using School_Management.API.Data;
+using School_Management.API.Exceptions;
+using School_Management.API.Models.Domain;
+using School_Management.API.Models.DTO;
+using School_Management.API.Repositories;
+
+namespace School_Management.API.Services
+{
+    public class AttendanceService : IAttendanceService
+    {
+        private readonly ApplicationDbContext context;
+        private readonly ITeacherRepository teacherRepository;
+
+        public AttendanceService(ApplicationDbContext context, ITeacherRepository teacherRepository)
+        {
+            this.context = context;
+            this.teacherRepository = teacherRepository;
+        }
+
+        public async Task<int> InsertAttendance(AttendanceRequest request)
+        {
+            var studentMaps = await context.StudentClassYear
+                                                   .AsNoTracking()
+                                                   .Where(x => x.ClassYearId == request.ClassYearId)
+                                                   .ToDictionaryAsync(x => x.StudentId, x => x.StudentClassYearId);
+            var infoAttendances = new List<Attendance>();
+            foreach (var item in request.InfoAttendances)
+            {
+                if(studentMaps.TryGetValue(item.StudentId, out var scy))
+                {
+                    infoAttendances.Add(new Attendance
+                    {
+                        Id = Guid.NewGuid(),
+                        Status = item.Status,
+                        Date = request.Date,
+                        Note = item.Note,
+                        StudentClassYearId = scy
+                    });
+                }
+                
+            }
+
+            if (!infoAttendances.Any()) return 0;
+
+            await context.Attendance.AddRangeAsync(infoAttendances);
+            return await context.SaveChangesAsync();
+        }
+        public async Task<int> AttendanceCheck(AttendanceRequest request, Guid userId)
+        {
+            //Check HomeRoom teacher
+            var teacherId = await teacherRepository.GetTeacherIdByUserId(userId);
+            var homeRoomId = await context.ClassYear.Where(x => x.Id == request.ClassYearId)
+                                                    .Select(g => g.HomeRoomId)
+                                                    .FirstOrDefaultAsync();
+            if (teacherId != homeRoomId) throw new ForbiddenException("Bạn không phải giáo viên chủ nhiệm của lớp học");
+
+            using var transaction = await context.Database.BeginTransactionAsync();
+            try
+            {
+                var existed = await context.Attendance
+                                           .AnyAsync(x => x.Date == request.Date && x.StudentClassYear.ClassYearId == request.ClassYearId);
+
+
+                int result;
+                if (existed == false)
+                {
+                    result = await InsertAttendance(request);
+
+                }
+                else
+                {
+                    var oldAttendances = await context.Attendance
+                                                      .Where(x => x.StudentClassYear.ClassYearId == request.ClassYearId
+                                                      && x.Date == request.Date)
+                                                      .ToListAsync();
+
+                    context.Attendance.RemoveRange(oldAttendances);
+                    result = await InsertAttendance(request);
+                }
+
+                if (result == 0) throw new BadRequestException("Cập nhật thất bại");
+                await transaction.CommitAsync();
+                return result;
+                
+
+            }
+            catch
+            {
+                await transaction.RollbackAsync();
+                throw;
+            }
+        }
+    }
+}
---
+++ code/be/School_Management.API/School_Management.API/Services/IAttendanceService.cs
@@ -0,0 +1,10 @@
+﻿using School_Management.API.Models.DTO;
+
+namespace School_Management.API.Services
+{
+    public interface IAttendanceService
+    {
+        //Upsert method
+        public Task<int> AttendanceCheck(AttendanceRequest request, Guid userId);
+    }
+}
