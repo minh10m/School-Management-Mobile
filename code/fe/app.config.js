07e93d01e5e90e4741b6935c7356d4336432025a
minh10m <minhhh1010@gmail.com>
2026-04-20 09:51:11 +0700

    feat: add update API configuration, and refactor app initialization sequence

--- code/fe/app.config.js
+++ code/fe/app.config.js
@@ -47,16 +47,8 @@ export default {
       "reactCompiler": true
     },
     "extra": {
-      "apiKey": process.env.API_KEY,
-      "authDomain": process.env.AUTH_DOMAIN,
-      "projectId": process.env.PROJECT_ID,
-      "storageBucket": process.env.STORAGE_BUCKET,
-      "messagingSenderId": process.env.MESSAGING_SENDER_ID,
-      "appId": process.env.APP_ID,
-      "eas": {
-        "projectId": process.env.EAS_PROJECT_ID
-      },
-      stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+      apiUrl: process.env.API_URL,
+      sepayApiKey: process.env.SEPAY_API_KEY,
     }
   }
 }
