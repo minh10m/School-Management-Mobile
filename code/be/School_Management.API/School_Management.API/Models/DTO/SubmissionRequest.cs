709f27e14693f72c07ca5429044659cf0a49f6d3
minh10m <minhhh1010@gmail.com>
2026-04-20 07:20:59 +0700

    fix

--- code/be/School_Management.API/School_Management.API/Models/DTO/SubmissionRequest.cs
+++ code/be/School_Management.API/School_Management.API/Models/DTO/SubmissionRequest.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace School_Management.API.Models.DTO
 {
@@ -8,7 +8,7 @@ namespace School_Management.API.Models.DTO
         public Guid AssignmentId { get; set; }
 
         [Required(ErrorMessage = "Đường dẫn file bài tập là bắt buộc")]
-        [Url(ErrorMessage = "Định dạng đường dẫn file không hợp lệ")]
+        [RegularExpression(@"^(https?|file|content)://.+", ErrorMessage = "Định dạng đường dẫn file không hợp lệ (Phải là link web hoặc file cục bộ)")]
         public string FileUrl { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Tiêu đề file bài tập là bắt buộc")]
