7a12aa83bc2ed2d2153731bcfce4c18bd00bf1f1
minh10m <minhhh1010@gmail.com>
2026-04-21 17:23:24 +0700

    fix

--- code/be/School_Management.API/School_Management.API/Models/DTO/CourseResponse.cs
+++ code/be/School_Management.API/School_Management.API/Models/DTO/CourseResponse.cs
@@ -1,4 +1,4 @@
-﻿namespace School_Management.API.Models.DTO
+namespace School_Management.API.Models.DTO
 {
     public class CourseResponse
     {
@@ -6,6 +6,7 @@
         public string CourseName { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public Guid TeacherSubjectId { get; set; }
+        public Guid SubjectId { get; set; }
         public string TeacherName { get; set; } = string.Empty;
         public string SubjectName { get; set; } = string.Empty;
         public string Status { get; set; } = string.Empty;
