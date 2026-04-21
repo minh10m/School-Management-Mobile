23329fd5d47608756967143a279867efd69c002a
TuanLee <23521711@gm.uit.edu.vn>
2026-04-21 09:09:56 +0700

    fix(result) : fix type result jush have 4 values, Mieng, 15p, Gk, Ck

--- code/be/School_Management.API/School_Management.API/EntityConfigurations/ResultConfiguration.cs
+++ code/be/School_Management.API/School_Management.API/EntityConfigurations/ResultConfiguration.cs
@@ -16,12 +16,14 @@ namespace School_Management.API.EntityConfigurations
             builder.Property(x => x.Weight).IsRequired();
             builder.Property(x => x.SchoolYear).IsRequired();
 
+
             builder.ToTable("Result", x =>
             {
                 x.HasCheckConstraint("CK_Value_Result", "\"Value\" >= 0 AND \"Value\" <= 10");
                 x.HasCheckConstraint("CK_Term_Result", "\"Term\" >= 1 AND \"Term\" <= 2");
                 x.HasCheckConstraint("CK_Weight_Result", "\"Weight\" >= 1 AND \"Weight\" <= 3");
                 x.HasCheckConstraint("CK_SchoolYear_Result", "\"SchoolYear\" > 2000 AND \"SchoolYear\" < 2100");
+                x.HasCheckConstraint("CK_Type_Result", "\"Type\" IN ('Miệng', '15 phút', 'Giữa kì', 'Cuối kì')");
             });
 
             builder.HasIndex(x => new { x.StudentId, x.SubjectId, x.Type, x.Term, x.SchoolYear }).IsUnique();
--- code/be/School_Management.API/School_Management.API/Models/DTO/UpdateResultRequest.cs
+++ code/be/School_Management.API/School_Management.API/Models/DTO/UpdateResultRequest.cs
@@ -5,7 +5,7 @@ namespace School_Management.API.Models.DTO
     public class UpdateResultRequest
     {
         [Required(ErrorMessage = "Loại điểm số là bắt buộc")]
-        [RegularExpression(@"^(?!\s*$).+", ErrorMessage = "Loại điểm số không được chỉ chứa khoảng trắng")]
+        [RegularExpression("^(Miệng|15 phút|Giữa kì|Cuối kì)$", ErrorMessage = "Loại điểm số không hợp lệ, chỉ được nhận giá trị là [Miệng], [15 phút], [Giữa kì] hoặc [Cuối kì]")]
         public string Type { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Điểm số là bắt buộc")]
@@ -15,10 +15,6 @@ namespace School_Management.API.Models.DTO
         [Required(ErrorMessage = "Học kì là bắt buộc")]
         [Range(1, 2, ErrorMessage = "Học kì nằm trong khoảng từ 1 tới 2")]
         public int Term { get; set; }
-
-        [Required(ErrorMessage = "Trọng số là bắt buộc")]
-        [Range(1, 3, ErrorMessage = "Trọng số nằm trong khoảng từ 1 tới 3")]
-        public int Weight { get; set; }
 
         [Required(ErrorMessage = "Năm học số là bắt buộc")]
         [Range(2000, 2100, ErrorMessage = "Năm học chỉ nằm trong khoảng từ 2000 tới 2100")]
